4b5117ca1408ad67fc875b4e968e382b06656c48
rohitcod3 <rohit.bangar9@gmail.com>
2024-11-29 14:39:17 +0530

    ui-fix-5

--- frontend/src/components/ChatRoom.jsx
+++ frontend/src/components/ChatRoom.jsx
@@ -104,14 +104,14 @@ export function ChatRoom() {
   };
   //h-[85vh] changed to h-[100vh]
   return (
-    <div className="chatroom h-screen flex justify-center items-center home-background overflow-y-auto">
+    <div className="chatroom h-screen flex justify-center items-center home-background overflow-hidden">
       <div className="h-[100vh] lg:h-[85vh] w-full max-w-[500px] sm:max-w-[600px] md:max-w-[700px] bg-purple-400 rounded-md bg-clip-padding backdrop-filter backdrop-blur-lg bg-opacity-10 border  border-gray-100">
         <div className="flex flex-col justify-between items-center shadow-lg w-full h-full p-5 rounded-md ">
           <h2 className="text-white text-lg sm:text-xl md:text-2xl mb-4 text-center">
             Course Chatroom: {courseId}
           </h2>
           <div
-            className="messages custom-scrollbar h-[75vh] sm:h-[65vh] md:h-[70vh] w-full overflow-y-auto p-3 rounded-md mb-4"
+            className="messages custom-scrollbar h-[75vh] sm:h-[65vh] md:h-[70vh] w-full overflow-y-auto p-3 rounded-md mb-4 relative"
             style={{ paddingBottom: "2rem" }}
           >
             {messages.length === 0 ? (
@@ -156,7 +156,7 @@ export function ChatRoom() {
               value={newMessage}
               onChange={(e) => setNewMessage(e.target.value)}
               onKeyDown={handleKeyPress}
-              className="flex-1 w-full sm:px-4 py-2 my-10 pr-10 rounded-md border border-gray-500 focus:outline-none"
+              className="flex-1 w-full sm:px-4 py-2 my-10 pr-10 rounded-md border border-gray-500 focus:outline-none absolute bottom-[22px]"
             />
             {/* Send Button for Desktop */}
             <button
@@ -168,7 +168,7 @@ export function ChatRoom() {
             {/* Arrow Icon for Mobile */}
             <button
               onClick={handleSendMessage}
-              className="absolute right-2 top-[49px] transform -translate-y-1/2 transform-translate-x-1 sm:hidden text-blue-500"
+              className="absolute right-[-2px] bottom-[40px] transform -translate-y-1/2 transform-translate-x-1 sm:hidden text-blue-500"
             >
               <svg
                 xmlns="http://www.w3.org/2000/svg"
